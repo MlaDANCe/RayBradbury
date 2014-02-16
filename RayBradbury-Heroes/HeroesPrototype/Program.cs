1d58d077eed956820b69fc2e6b17ee523bcd8094
Imraotnh <nkar@mail.bg>
2014-02-16 16:20:38 +0200

    // ?

--- RayBradbury-Heroes/HeroesPrototype/Program.cs
+++ RayBradbury-Heroes/HeroesPrototype/Program.cs
@@ -12,7 +12,7 @@ namespace HeroesPrototype
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() // try
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
