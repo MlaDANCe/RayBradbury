cb17b9bb2ccf04502f601cd8f492b46b9f792f65
pepinho24 <p_mil4ev@hotmail.com>
2014-02-24 16:47:13 +0200

    added Wood and Ore classes

---
+++ HeroesPrototype/Ore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using HeroesPrototype.Geometry;
+using HeroesPrototype.Items;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace HeroesPrototype
+{
+    internal class Ore:Item, IDrawable
+	{
+        private readonly List<int> orePileValue = new List<int> { 500, 600, 700, 800, 900, 1000 };
+        private Random randomOrePileValue = new Random();
+        private readonly Bitmap sprite;
+
+        public Point2D Origin { get; set; }
+
+        public Size2D Size { get; private set; }
+
+        public Ore(Point2D origin, Bitmap sprite)
+            : base("wood", origin)
+        {
+            base.Quantity = orePileValue[randomOrePileValue.Next(orePileValue.Count)];
+            this.Origin = origin;
+            this.sprite = sprite;
+        }
+
+        public Bitmap GetSprite()
+        {
+            return this.sprite;
+        }
+
+		public Ore(Point2D origin) : base("ore", origin)
+		{
+            base.Quantity = orePileValue[randomOrePileValue.Next(orePileValue.Count)];
+		}
+	}
+}
---
+++ HeroesPrototype/Wood.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using HeroesPrototype.Geometry;
+using HeroesPrototype.Items;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace HeroesPrototype
+{
+    internal class Wood :Item, IDrawable
+	{
+        private readonly List<int> woodPileValue = new List<int> { 500, 600, 700, 800, 900, 1000 };
+        private Random randomWoodPileValue = new Random();
+        private readonly Bitmap sprite;
+
+        public Point2D Origin { get; set; }
+
+        public Size2D Size { get; private set; }
+
+        public Wood(Point2D origin, Bitmap sprite)
+            : base("wood", origin)
+        {
+            base.Quantity = woodPileValue[randomWoodPileValue.Next(woodPileValue.Count)];
+            this.Origin = origin;
+            this.sprite = sprite;
+        }
+
+        public Bitmap GetSprite()
+        {
+            return this.sprite;
+        }
+
+		public Wood(Point2D origin) : base("wood", origin)
+		{
+            base.Quantity = woodPileValue[randomWoodPileValue.Next(woodPileValue.Count)];
+		}
+	}
+}
