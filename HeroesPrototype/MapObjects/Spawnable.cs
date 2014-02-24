bbac9e0c1d02e30b7bf6a0fc3906ef8d2a620dbc
nikolap04 <nikolap04@gmail.com>
2014-02-24 22:41:30 +0200

    Spawnable.cs added

---
+++ HeroesPrototype/MapObjects/Spawnable.cs
@@ -0,0 +1,18 @@
+﻿using HeroesPrototype.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroesPrototype.MapObjects
+{
+    interface Spawnable
+    {
+        bool IsSpawnable { get; set; }
+
+        List<Unit> UnitsSpawned {  get;  set; }
+
+        List<Unit> BuyUnits(int gold);
+    }
+}
