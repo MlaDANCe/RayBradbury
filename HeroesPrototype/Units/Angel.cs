1e25a4b38e5c630badcdbbdc961354daa60f4808
Imraotnh <nkar@mail.bg>
2014-02-25 03:24:13 +0200

    modified angel, marksman and pikeman classes

--- HeroesPrototype/Units/Angel.cs
+++ HeroesPrototype/Units/Angel.cs
@@ -6,9 +6,18 @@ using System.Drawing;
 namespace HeroesPrototype
 {
 	public class Angel : UnitLevel3
-	{       
+	{
+        private readonly Bitmap sprite;
+
 		public Angel(Point2D origin) : base("Angel", origin, 10, 10, 10)
 		{
 		}        
+
+        public Angel(Point2D origin, Bitmap sprite)
+            : base("Angel", origin, 10, 10, 10)
+        {
+            this.Origin = origin;
+            this.sprite = sprite;
+        }
 	}
 }
