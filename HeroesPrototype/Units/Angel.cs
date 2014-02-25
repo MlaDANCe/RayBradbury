7f46f13650d189a3eb43543fb61bf39b38c1fcb5
Imraotnh <nkar@mail.bg>
2014-02-25 03:20:33 +0200

    added sprites for angel, marksman, pikeman

--- HeroesPrototype/Units/Angel.cs
+++ HeroesPrototype/Units/Angel.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Linq;
 using HeroesPrototype.Geometry;
+using System.Drawing;
 
 namespace HeroesPrototype
 {
 	public class Angel : UnitLevel3
-	{
+	{       
 		public Angel(Point2D origin) : base("Angel", origin, 10, 10, 10)
 		{
-		}
+		}        
 	}
 }
