40e05393fbb3789f95f822513fdbaf129cfc979d
pepinho24 <p_mil4ev@hotmail.com>
2014-02-26 20:25:37 +0200

    Some styling in code

--- HeroesPrototype/Trainer.cs
+++ HeroesPrototype/Trainer.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Windows.Forms;
+
     public partial class Trainer : Form
     {
         private MainScene mainScene;
 
         public Trainer(MainScene mainScene)
         {
-            InitializeComponent();
+            this.InitializeComponent();
             this.mainScene = mainScene;
         }
 
@@ -16,28 +17,28 @@
         {
             int number = 0;
             int uintNum = 0;
-            if (int.TryParse(txtDefence.Text, out number)) mainScene.MainCharacter.Defense += number;
-            if (int.TryParse(TxtAttack.Text, out number)) mainScene.MainCharacter.Attack += number;
-            if (int.TryParse(txtGold.Text, out uintNum)) mainScene.MainCharacter.Gold += uintNum;
-            if (int.TryParse(txtWood.Text, out uintNum)) mainScene.MainCharacter.Wood += uintNum;
-            if (int.TryParse(txtOre.Text, out uintNum)) mainScene.MainCharacter.Ore += uintNum;
-            if (int.TryParse(txtMoves.Text, out number)) mainScene.MainCharacter.Moves += number;
-            if (int.TryParse(txtMaxMoves.Text, out number)) mainScene.MainCharacter.MaxMoves += number;
-            if (int.TryParse(txtXp.Text, out uintNum)) mainScene.MainCharacter.Experience += uintNum;
+            if (int.TryParse(txtDefence.Text, out number)) this.mainScene.MainCharacter.Defense += number;
+            if (int.TryParse(TxtAttack.Text, out number)) this.mainScene.MainCharacter.Attack += number;
+            if (int.TryParse(txtGold.Text, out uintNum)) this.mainScene.MainCharacter.Gold += uintNum;
+            if (int.TryParse(txtWood.Text, out uintNum)) this.mainScene.MainCharacter.Wood += uintNum;
+            if (int.TryParse(txtOre.Text, out uintNum)) this.mainScene.MainCharacter.Ore += uintNum;
+            if (int.TryParse(txtMoves.Text, out number)) this.mainScene.MainCharacter.Moves += number;
+            if (int.TryParse(txtMaxMoves.Text, out number)) this.mainScene.MainCharacter.MaxMoves += number;
+            if (int.TryParse(txtXp.Text, out uintNum)) this.mainScene.MainCharacter.Experience += uintNum;
         }
 
         private void btnChangeTo_Click(object sender, EventArgs e)
         {
             int number = 0;
             int uintNum = 0;
-            if (int.TryParse(txtDefence.Text, out number)) mainScene.MainCharacter.Defense = number;
-            if (int.TryParse(TxtAttack.Text, out number)) mainScene.MainCharacter.Attack = number;
-            if (int.TryParse(txtGold.Text, out uintNum)) mainScene.MainCharacter.Gold = uintNum;
-            if (int.TryParse(txtWood.Text, out uintNum)) mainScene.MainCharacter.Wood = uintNum;
-            if (int.TryParse(txtOre.Text, out uintNum)) mainScene.MainCharacter.Ore = uintNum;
-            if (int.TryParse(txtMoves.Text, out number)) mainScene.MainCharacter.Moves = number;
-            if (int.TryParse(txtMaxMoves.Text, out number)) mainScene.MainCharacter.MaxMoves = number;
-            if (int.TryParse(txtXp.Text, out uintNum)) mainScene.MainCharacter.Experience = uintNum;
+            if (int.TryParse(txtDefence.Text, out number)) this.mainScene.MainCharacter.Defense = number;
+            if (int.TryParse(TxtAttack.Text, out number)) this.mainScene.MainCharacter.Attack = number;
+            if (int.TryParse(txtGold.Text, out uintNum)) this.mainScene.MainCharacter.Gold = uintNum;
+            if (int.TryParse(txtWood.Text, out uintNum)) this.mainScene.MainCharacter.Wood = uintNum;
+            if (int.TryParse(txtOre.Text, out uintNum)) this.mainScene.MainCharacter.Ore = uintNum;
+            if (int.TryParse(txtMoves.Text, out number)) this.mainScene.MainCharacter.Moves = number;
+            if (int.TryParse(txtMaxMoves.Text, out number)) this.mainScene.MainCharacter.MaxMoves = number;
+            if (int.TryParse(txtXp.Text, out uintNum)) this.mainScene.MainCharacter.Experience = uintNum;
         }
 
         private void btnClearTxt_Click(object sender, EventArgs e)
