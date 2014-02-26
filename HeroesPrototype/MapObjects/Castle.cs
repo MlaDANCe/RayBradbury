26b5ed331792ad0df8a42a20e206ed655af69ed6
Imraotnh <nkar@mail.bg>
2014-02-26 05:04:12 +0200

    added more units in the castle

--- HeroesPrototype/MapObjects/Castle.cs
+++ HeroesPrototype/MapObjects/Castle.cs
@@ -11,10 +11,24 @@ namespace HeroesPrototype.MapObjects
             : base(origin, sprite)
         {
             this.UnitsSpawned = new List<Unit>();
+
             this.UnitsSpawned.Add(new Magog(new Point2D(0, 0)));
             this.UnitsSpawned.Add(new Magog(new Point2D(0, 0)));
-            this.UnitsSpawned.Add(new Magog(new Point2D(0, 0)));
-            this.UnitsSpawned.Add(new Magog(new Point2D(0, 0)));
+
+            this.UnitsSpawned.Add(new Angel(new Point2D(0, 0)));
+            this.UnitsSpawned.Add(new Angel(new Point2D(0, 0)));
+            
+            this.UnitsSpawned.Add(new Devil(new Point2D(0, 0)));
+            this.UnitsSpawned.Add(new Devil(new Point2D(0, 0)));
+            
+            this.UnitsSpawned.Add(new Marksman(new Point2D(0, 0)));
+            this.UnitsSpawned.Add(new Marksman(new Point2D(0, 0)));
+
+            this.UnitsSpawned.Add(new Pikeman(new Point2D(0, 0)));
+            this.UnitsSpawned.Add(new Pikeman(new Point2D(0, 0)));
+
+            this.UnitsSpawned.Add(new Imp(new Point2D(0, 0)));
+            this.UnitsSpawned.Add(new Imp(new Point2D(0, 0)));
         }
         public bool IsSpawnable { get; set; }
 
